0f9b72c0ec25bb688bc49cd2df57e772bc60909f
Tiyas Banerjee <181446532+Tiyasbanerjee@users.noreply.github.com>
2026-08-17 06:11:48 +0530

    this two files are needed for npm to work.

--- index.js
+++ index.js
@@ -44,16 +44,17 @@ app.command("/raju-catfact",async({ack,respond})=>{
     }
 });
 
-app.command("raju-jike",async({ack,respond})=>{
+app.command("/raju-joke",async({ack,respond})=>{
     await ack();
     try{
         const response = await axios.get("https://official-joke-api.appspot.com/random_joke");
         await respond({
             text:
             `${response.data.setup}
-            
-            `
-        })
+            ${response.data.punchline}`
+        }) 
+    } catch(err){
+        await respond({text:"failed to fetch joke"});
     }
 })
 
